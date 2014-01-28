9e8fac49a6ca1deb7771e324c51da2aeb0545089
Markus Amalthea Magnuson <markus.magnuson@gmail.com>
2014-01-29 00:09:44 +0100

    Add missing website links.

--- js/script.js
+++ js/script.js
@@ -26,7 +26,10 @@ $(document).ready(function () {
 		5196226: '/disco-143-virtuoso.html',
 		5199356: '/disco-175-geminiani-albinoni-vivaldi.html',
 		5202488: '/en/disco-145-yumi-yamagata---dreaming-venice.html',
-		5204682: '/disco-140-il-cimento-dellarmonia-e-dellinvenzione-i-le-quattro-stagioni.html'
+		5204682: '/disco-140-il-cimento-dellarmonia-e-dellinvenzione-i-le-quattro-stagioni.html',
+		5315345: '/disco-138-allegro-con-fuoco.html',
+		5315533: '/disco-172-bravo.html',
+		5340346: '/disco-147-il-cimento-dellarmonia-e-dellinvenzione-ii.html'
 	};
 
 	// This is a general purpose function for making Ajax calls over JSONP with
