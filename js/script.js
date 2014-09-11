4053333d51922d42f59837f3542106c0022cada1
Markus Amalthea Magnuson <markus.magnuson@gmail.com>
2014-09-11 10:39:02 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -108,8 +108,8 @@ $(document).ready(function () {
 			// It's not allowed to hotlink their images, and we can't
 			// authenticate as needed for image requests, since this is
 			// client-side code and any OAuth secret would be... not secret.
-			// Before: http://api.discogs.com/image/R-150-5340346-1390948761-8433.jpeg
-			var thumb = release.thumb.replace('http://api.discogs.com/image/', '/img/thumbs/');
+			// Before: http://api.discogs.com/images/R-90-5195481-1387129188-3089.jpeg
+			var thumb = release.thumb.replace('http://api.discogs.com/images/', '/img/thumbs/');
 			// After: /img/thumbs/R-150-5340346-1390948761-8433.jpeg
 
 			elementsToAdd.push('' +
